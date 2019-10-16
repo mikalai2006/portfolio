580105c85782388c12678b2784567b72499ad348
mikalai2006 <composer.opencart@gmail.com>
2019-10-16 19:22:50 +0300

    feat: add projects

---
+++ projects/theyalow/assets/btn.js
@@ -0,0 +1,71 @@
+const btn = document.querySelectorAll('.btn');
+const wrapper = document.querySelector('.wrapper');
+const btnTypex = document.querySelectorAll('.typex');
+const typeClassBtnDefault = 'btn-primary';
+const typeClassBtnActive = 'd-none';
+
+
+function changeActiveBtn(type) {
+
+    btnTypex.forEach(function (button) {
+        button.classList.remove(typeClassBtnActive);
+        button.classList.add(typeClassBtnDefault);
+    });
+    const activeBtn = document.querySelector('[data-typex='+ type +']');
+    activeBtn.classList.add(typeClassBtnActive);
+    activeBtn.classList.remove(typeClassBtnDefault);
+
+}
+
+function changeMode(typex) {
+
+    if (typex === 'desktop'){
+        wrapper.style.width = '100%';
+        changeActiveBtn(typex);
+    } else if (typex === 'mobile') {
+        wrapper.style.width = '640px';
+        changeActiveBtn(typex);
+    } else {
+        window.history.back();
+    }
+
+}
+
+btn.forEach(function(button) {
+
+    button.addEventListener('click', function() {
+        const type = this.getAttribute('data-typex');
+        changeMode(type);
+    });
+
+})
+
+function toggleBtn() {
+
+    if (window.innerWidth < 900) {
+        
+        btnTypex.forEach(function(button) {
+            button.style.display = 'none';
+        });
+
+        changeMode('mobile');
+
+    } else {
+
+        btnTypex.forEach(function(button) {
+            button.style.display = 'block';
+        });
+
+        changeMode('desktop');
+
+    }
+}
+
+toggleBtn();
+
+window.addEventListener('resize', function() {
+
+    toggleBtn();
+    
+})
+
